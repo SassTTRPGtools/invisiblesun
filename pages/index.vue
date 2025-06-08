7d5abd3f7368578d76095d0126a8dc6490a7c512
SassTTRPGtools <pechin1219@gmail.com>
2025-06-08 04:55:46 +0000

    init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>歡迎來到 Nuxt 3！</h1>
+  <p>這是首頁，Nuxt 基本環境已建置完成。</p>
+</template>
+
+<script setup lang="ts">
+// 這裡可以加入組件邏輯
+</script>
+
+<style scoped>
+h1 {
+  color: #00dc82;
+}
+</style>
